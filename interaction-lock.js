f1e1cb1e0e4cebeddd479cc3c5cbeb46221150eb
Paul Colleoni <paul.colleoni@gmail.com>
2016-02-29 16:55:57 +0900

    lint

--- interaction-lock.js
+++ interaction-lock.js
@@ -5,35 +5,35 @@ var lockNodes = {};
 var lockId = 0;
 
 function isLocked(DOMNode) {
-  for (var lockId in lockNodes) {
-    var node = lockNodes[lockId];
-    while (node !== null) {
-      if (node === DOMNode) { return true; }
-      node = node.parentNode;
-    }
-  }
+	for (var id in lockNodes) {
+		var node = lockNodes[id];
+		while (node !== null) {
+			if (node === DOMNode) { return true; }
+			node = node.parentNode;
+		}
+	}
 
-  return false;
+	return false;
 }
 emitter.isLocked = isLocked;
 
 function requestLockOn(DOMNode) {
-  if (isLocked(DOMNode)) { return null; }
+	if (isLocked(DOMNode)) { return null; }
 
-  lockId += 1;
-  lockNodes[lockId] = DOMNode;
-  emitter.emit('lock', DOMNode);
-  return lockId;
+	lockId += 1;
+	lockNodes[lockId] = DOMNode;
+	emitter.emit('lock', DOMNode);
+	return lockId;
 }
 emitter.requestLockOn = requestLockOn;
 
 function releaseLock(id) {
-  const DOMNode = lockNodes[id];
-  if (DOMNode) {
-    delete lockNodes[id];
-    lockId -= 1;
-    emitter.emit('unlock', DOMNode);
-  }
+	const DOMNode = lockNodes[id];
+	if (DOMNode) {
+		delete lockNodes[id];
+		lockId -= 1;
+		emitter.emit('unlock', DOMNode);
+	}
 }
 emitter.releaseLock = releaseLock;
 
