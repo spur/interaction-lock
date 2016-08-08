dd4f782c8b04a252f88b8436926272b7501e9c7b
Paul Colleoni <paul.colleoni@gmail.com>
2016-08-08 14:13:15 +0900

    added a container lock that locks any child of a specified container

--- interaction-lock.js
+++ interaction-lock.js
@@ -2,6 +2,7 @@ var EventEmitter = require('events').EventEmitter;
 
 var emitter = new EventEmitter();
 var lockNodes = {};
+var lockContainerNodes = {};
 var lockId = 0;
 
 function isLocked(DOMNode) {
@@ -10,6 +11,15 @@ function isLocked(DOMNode) {
 		while (node !== null) {
 			if (node === DOMNode) { return true; }
 			node = node.parentNode;
+		}
+	}
+
+	for (var id in lockContainerNodes) {
+		var container = lockContainerNodes[id];
+		var parent = DOMNode;
+		while (parent !== null) {
+			if (parent === container) { return true; }
+			parent = parent.parentNode;
 		}
 	}
 
@@ -27,10 +37,21 @@ function requestLockOn(DOMNode) {
 }
 emitter.requestLockOn = requestLockOn;
 
+function requestContainerLockOn(DOMNode) {
+	if (isLocked(DOMNode)) { return null; }
+
+	lockId += 1;
+	lockContainerNodes[lockId] = DOMNode;
+	emitter.emit('container-lock', DOMNode);
+	return lockId;
+}
+emitter.requestContainerLockOn = requestContainerLockOn;
+
 function releaseLock(id) {
-	const DOMNode = lockNodes[id];
+	var DOMNode = lockNodes[id] || lockContainerNodes[id];
 	if (DOMNode) {
 		delete lockNodes[id];
+		delete lockContainerNodes[id];
 		lockId -= 1;
 		emitter.emit('unlock', DOMNode);
 	}
